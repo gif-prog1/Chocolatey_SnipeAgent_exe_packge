cc9ce12560b748c38cb205739063547961b92c21
gif-prog1 <109793088+gif-prog1@users.noreply.github.com>
2024-05-27 23:39:10 +0530

    Initial Upload

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'snipeagent'
+$url = 'http://192.168.3.15:88/SnipeAgent1.zip'
+$zipDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$extractedDir = Join-Path $zipDir "$packageName"
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $extractedDir
+  fileType       = 'zip'
+  url            = $url
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$exePath = Get-ChildItem $extractedDir -Filter "*.exe" | Select-Object -ExpandProperty FullName
+
+if ($exePath) {
+  Install-ChocolateyInstallPackage -PackageName $packageName -FileType 'exe' -SilentArgs '/S' -File "$exePath"
+} else {
+  Write-Error "Failed to find .exe file in the extracted directory."
+}
